d9c3bf16cc3ede0f2d463534714d4942e28963f0
Biagio Dev <biagio@biag.dev>
2022-09-28 07:06:00 -0400

    refactored optionContainer rendering

--- App.js
+++ App.js
@@ -3,27 +3,26 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View, SafeAreaView } from 'react-native';
 import styles from './App.styles';
 import OptionContainer from './src/components/OptionContainer';
+// import imageMultipleChoiceQuestions from './assets/data/imageMultipleChoiceQuestions';
+import question from './assets/data/oneQuestionWithOption';
 
 const App = () => (
   <SafeAreaView style={styles.root}>
-    <Text style={styles.title}>Which of these is a "glass"?</Text>
-
+    <Text style={styles.title}>{question.question}</Text>
     <View style={styles.optionsContainer}>
-      <OptionContainer
-        image={
-          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png'
-        }
-        text={'cup'}
-      />
-      <OptionContainer
-        image={
-          'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png'
-        }
-        text={'glass'}
-      />
+      {question.options.map(option => (
+        <OptionContainer
+          key={option.key}
+          image={option.image}
+          text={option.text}
+        />
+      ))}
     </View>
     <StatusBar style='auto' />
   </SafeAreaView>
 );
 
 export default App;
+
+{
+}
