253aeba6da0073b5f078bd0c3ee9ea44238ffc1d
Biagio Dev <biagio@biag.dev>
2022-10-06 14:32:25 -0400

    implemented conditional rendering/ lives logic

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { SafeAreaView, Alert } from 'react-native';
 import styles from './App.styles';
 import ImageMulitpleChoiceQuestions from './src/components/ImageMulitpleChoiceQuestions/ImageMulitpleChoiceQuestions';
 import OpenEndedQuestion from './src/components/OpenEndedQuestion/OpenEndedQuestion';
-
-import questions from './assets/data/openEndedQuestions';
+import Header from './src/components/Header';
+import questions from './assets/data/allQuestions';
 
 const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [lives, setLives] = useState(5);
 
   useEffect(() => {
     if (currentQuestionIndex >= questions.length) {
@@ -24,22 +25,45 @@ const App = () => {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
+  const restart = () => {
+    setCurrentQuestionIndex(0);
+    setLives(5);
+  };
+
   const onWrong = () => {
-    Alert.alert('Wrong!');
+    if (lives <= 1) {
+      Alert.alert('Game Over!', 'Try Again', [
+        {
+          text: 'Start Over',
+          onPress: restart,
+        },
+      ]);
+    } else {
+      Alert.alert('Wrong!');
+      setLives(lives - 1);
+    }
   };
 
   return (
     <SafeAreaView style={styles.root}>
-      {/* <ImageMulitpleChoiceQuestions
-        currentQuestion={currentQuestion}
-        onCorrect={onCorrect}
-        onWrong={onWrong}
-      /> */}
-      <OpenEndedQuestion
-        currentQuestion={currentQuestion}
-        onCorrect={onCorrect}
-        onWrong={onWrong}
+      <Header
+        progress={currentQuestionIndex / questions.length}
+        lives={lives}
       />
+      {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' && (
+        <ImageMulitpleChoiceQuestions
+          currentQuestion={currentQuestion}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+      )}
+      {currentQuestion.type === 'OPEN_ENDED' && (
+        <OpenEndedQuestion
+          currentQuestion={currentQuestion}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+      )}
       <StatusBar style='auto' />
     </SafeAreaView>
   );
